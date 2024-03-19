21bb62cd35430aa72661562e317f83ce6ae05bad
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2024-03-19 19:33:48 -0400

    Update index30f430f4.js

--- js/index30f430f4.js
+++ js/index30f430f4.js
@@ -140,19 +140,27 @@ var app = new Vue({
   const totalSupply = await erc20Contract.methods.totalSupply().call();
   const userBalance = await erc20Contract.methods.balanceOf(this.metamaskAccount).call();
   const reserves = await erc20Contract.methods.getReserves().call();
-  const reserve1 = reserves._reserve1; // Assuming _reserve1 represents token1
+  const reserve0 = reserves._reserve0;
+  const reserve1 = reserves._reserve1; 
   console.log('reserve1:', reserve1);
-
+  const rewardProportion = calculateEggSell /totalSupply
+  console.log('proportionReward:', rewardProportion);
   const proportion = userBalance / totalSupply;
   console.log('proportion:', proportion);
+  const reserve0Adjusted = reserve0 /1e6;
+  console.log('reserve0Adjusted', reserve0Adjusted);
   const reserve1Adjusted = reserve1 / 1e6;
-  console.log('reserve1Adjusted', reserve1Adjusted);//got this to be correct amount
+  console.log('reserve1Adjusted', reserve1Adjusted)
+  const token0Value = Math.floor(reserve0Adjusted) * proportion;
+  console.log('token0value:', token0Value);
   const token1Value = Math.floor(reserve1Adjusted) * proportion;
   console.log('token1value:', token1Value);
+  const token0ValueX2 = token0Value *2;
   const token1ValueX2 = token1Value *2;
-
+  const token0ValueWithDecimals = parseFloat(token0ValueX2).toFixed(6);
+  console.log('token0value with decimals:', token0ValueWithDecimals);
   const token1ValueWithDecimals = parseFloat(token1ValueX2).toFixed(2);
-  console.log('token1value with deci:', token1ValueWithDecimals);
+  console.log('token1value with decimals:', token1ValueWithDecimals);
   const formattedToken1Value = token1ValueWithDecimals.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
 console.log('Formatted token1 value:', formattedToken1Value);

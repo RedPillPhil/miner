2ff5ec536adeea43e8586d96c0178cb919ab6c18
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2024-03-20 12:45:44 -0400

    Update index30f430f4.js

--- js/index30f430f4.js
+++ js/index30f430f4.js
@@ -111,16 +111,21 @@ methods: {
   },
 updateLPAmount() {
   console.log('Updating LP Amount...');
+  console.log('Balance:', this.balance);
+  console.log('Percentage:', this.percentage);
   // Calculate buy amount based on percentage and wallet LP value
   this.buyAmount = this.calculateBuyAmount();
 },
 calculateBuyAmount() {
   console.log('Calculating buy amount...');
+  console.log('Balance:', this.balance);
+  console.log('Percentage:', this.percentage);
   // Calculate buy amount based on percentage and wallet LP value
   const calculatedAmount = (this.balance * this.percentage) / 100;
   console.log('Calculated amount:', calculatedAmount);
   return calculatedAmount;
 },
+
 
     async onConnect() {
       try {

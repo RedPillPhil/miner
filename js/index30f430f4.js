838f65fd3cc5037af43b13e1c1d43f7fa14ec9d4
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2024-03-19 23:03:59 -0400

    Update index30f430f4.js

--- js/index30f430f4.js
+++ js/index30f430f4.js
@@ -171,9 +171,9 @@ console.log('Formatted token1 value:', formattedToken1Value);
 
       this.contractInstance = new this.web3Object.eth.Contract(contractABI, contractAddress)
 
-      this.readValues()
+      this.readValues(totalSupply);
     },
-async readValues() {
+async readValues(totalSupply) {
   const web3 = new Web3('HTTP://127.0.0.1:1923 ');
   // const web3 = new Web3('https://data-seed-prebsc-2-s1.bnbchain.org:8545');
   let instance = new web3.eth.Contract(contractABI, contractAddress);
